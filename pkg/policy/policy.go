b926733b236b04256c437fddb3c90fd236b1b368
root <root@127.0.1.1>
2021-11-16 17:39:36 +0900

    scheduler backup

--- pkg/policy/policy.go
+++ pkg/policy/policy.go
@@ -8,54 +8,8 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	util "Hybrid_Cluster/hcp-scheduler/pkg/util"
 	cobrautil "Hybrid_Cluster/hybridctl/util"
 )
-
-func GetWatchingLevel() util.WatchingLevel {
-	master_config, _ := cobrautil.BuildConfigFromFlags("kube-master", "/root/.kube/config")
-	clientset, err := v1alpha1.NewForConfig(master_config)
-	hcppolicy, err := clientset.HCPPolicy("hcp").Get("watching-level", metav1.GetOptions{})
-	if err != nil {
-		log.Println(err)
-	}
-
-	var watchingLevel util.WatchingLevel
-	watchingLevel.Levels = make([]util.Level, 5)
-	for i, policy := range hcppolicy.Spec.Template.Spec.Policies {
-		var level util.Level
-		level = util.Level{
-			Type:  policy.Type,
-			Value: policy.Value,
-		}
-		watchingLevel.Levels[i] = level
-	}
-	return watchingLevel
-}
-
-func GetWarningLevel() util.Level {
-	master_config, _ := cobrautil.BuildConfigFromFlags("kube-master", "/root/.kube/config")
-	clientset, err := v1alpha1.NewForConfig(master_config)
-	hcppolicy, err := clientset.HCPPolicy("hcp").Get("warning-level", metav1.GetOptions{})
-	if err != nil {
-		log.Println(err)
-	}
-
-	// for i, policy := range hcppolicy.Spec.Template.Spec.Policies {
-	policies := hcppolicy.Spec.Template.Spec.Policies
-	if len(policies) == 1 {
-		level := util.Level{
-			Type:  hcppolicy.Spec.Template.Spec.Policies[0].Type,
-			Value: hcppolicy.Spec.Template.Spec.Policies[0].Value,
-		}
-		return level
-	} else {
-		level := util.Level{
-			Type: "none",
-		}
-		return level
-	}
-}
 
 func GetCycle() float64 {
 	master_config, _ := cobrautil.BuildConfigFromFlags("kube-master", "/root/.kube/config")
