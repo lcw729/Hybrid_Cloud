2f7021b7e271670246a286d338734ee836f16b1e
lcw729 <lcw729729@gmail.com>
2022-02-23 16:40:02 +0900

    create subtree hcp-apiserver

--- Hcp-apiserver/2.create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# kubectl create ns hcp
-kubectl create -f deploy/operator.yaml
-# kubectl create -f deploy/pv.yaml
-# kubectl create -f deploy/pvc.yaml
-kubectl create -f deploy/role_binding.yaml
-kubectl create -f deploy/service_account.yaml
-kubectl create -f deploy/service.yaml
-# kubectl create -f deploy/operator.yaml
