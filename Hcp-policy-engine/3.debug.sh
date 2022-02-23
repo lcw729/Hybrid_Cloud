7f6c7059f4a965e35d66fabf43b47d537a219266
lcw729 <lcw729729@gmail.com>
2022-02-23 17:11:08 +0900

    add subtree hybridctl

--- Hcp-policy-engine/3.debug.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-NS=hybrid
-
-NAME=$(kubectl get pod -n $NS | grep -E 'hcp-policy-engine' | awk '{print $1}')
-
-echo "Exec Into '"$NAME"'"
-
-#kubectl exec -it $NAME -n $NS /bin/sh
-kubectl logs -n $NS $NAME
