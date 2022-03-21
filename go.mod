0a3a7ded4ef22e0c8bcbfc16b1898792dd1b4233
lcw729 <lcw729729@gmail.com>
2022-03-21 18:19:33 +0900

    scheduler get metrics

--- go.mod
+++ go.mod
@@ -33,6 +33,7 @@ require (
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65
 	k8s.io/kube-scheduler v0.23.4
 	k8s.io/kubernetes v1.23.4
+	k8s.io/metrics v0.23.4
 	k8s.io/sample-controller v0.23.4
 	sigs.k8s.io/aws-iam-authenticator v0.5.5
 	sigs.k8s.io/controller-runtime v0.11.1
@@ -43,7 +44,6 @@ replace (
 	github.com/go-logr/logr => github.com/go-logr/logr v0.4.0
 	k8s.io/api => k8s.io/api v0.23.4
 	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.22.4
-	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.10.3
 	k8s.io/apimachinery => k8s.io/apimachinery v0.22.4
 	k8s.io/apiserver => k8s.io/apiserver v0.23.0-alpha.4
 	k8s.io/cli-runtime => k8s.io/cli-runtime v0.23.0-alpha.4
@@ -68,5 +68,6 @@ replace (
 	k8s.io/mount-utils => k8s.io/mount-utils v0.23.4
 	k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.23.4
 	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.23.4
+	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.10.3
 // sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.6.0
 )
--- hcp-scheduler/pkg/resourceinfo/resource_info.go
+++ hcp-scheduler/pkg/resourceinfo/resource_info.go
@@ -1,12 +1,21 @@
 package resourceinfo
 
 import (
+	cobrautil "Hybrid_Cloud/hybridctl/util"
 	"Hybrid_Cloud/pkg/apis/hcpcluster/v1alpha1"
+	hcpclusterv1alpha1 "Hybrid_Cloud/pkg/client/hcpcluster/v1alpha1/clientset/versioned"
+	"context"
+	"fmt"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/kubernetes"
+	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
 )
+
+type ClusterInfoList []*ClusterInfo
 
 type ClusterInfo struct {
 	ClusterName        string
@@ -14,7 +23,90 @@ type ClusterInfo struct {
 	RequestedResources *Resources
 	AllocableResources *Resources
 	CapacityResources  *Resources
-	ClusterList        *v1alpha1.HCPClusterList
+}
+
+func JoinClusterList() ([]v1alpha1.HCPCluster, error) {
+
+	var joinCluster_list []v1alpha1.HCPCluster
+	config, err := cobrautil.BuildConfigFromFlags("kube-master", "/root/.kube/config")
+	if err != nil {
+		fmt.Println("this error")
+		return nil, err
+	}
+
+	cluster_client := hcpclusterv1alpha1.NewForConfigOrDie(config)
+
+	cluster_list, err := cluster_client.HcpV1alpha1().HCPClusters("hcp").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Println("this error")
+		return nil, err
+	}
+
+	for _, hcpcluster := range cluster_list.Items {
+		if hcpcluster.Spec.JoinStatus == "JOIN" {
+			joinCluster_list = append(joinCluster_list, hcpcluster)
+		}
+	}
+
+	return joinCluster_list, nil
+}
+
+func NewClusterInfoList() ClusterInfoList {
+
+	var clusterInfo_list ClusterInfoList
+	joinCluster_list, err := JoinClusterList()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	for _, hcpcluster := range joinCluster_list {
+		cluster_name := hcpcluster.Name
+		clusterInfo := ClusterInfo{
+			ClusterName: cluster_name,
+			Nodes:       GetNodeInfo(cluster_name),
+		}
+		clusterInfo_list = append(clusterInfo_list, &clusterInfo)
+
+	}
+
+	return clusterInfo_list
+}
+
+func GetNodeInfo(clusterName string) []*NodeInfo {
+	var nodeInfo []*NodeInfo
+	config, err := cobrautil.BuildConfigFromFlags(clusterName, "/root/.kube/config")
+	if err != nil {
+		fmt.Println("this error")
+		return nil
+	}
+
+	cluster_client := kubernetes.NewForConfigOrDie(config)
+
+	_, err = cluster_client.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Println("this error")
+		return nil
+	}
+	return nodeInfo
+
+}
+
+func NodeMetrics(clusterName string) {
+	config, err := cobrautil.BuildConfigFromFlags(clusterName, "/root/.kube/config")
+	if err != nil {
+		fmt.Println("this error")
+	}
+
+	mc, err := metrics.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// mc.MetricsV1beta1().NodeMetricses().Get(cotex"your node name", metav1.GetOptions{})
+	nodeMetrics_list, err := mc.MetricsV1beta1().NodeMetricses().List(context.TODO(), metav1.ListOptions{})
+	fmt.Println(nodeMetrics_list.Items[0].Usage)
+
 }
 
 // PodInfo is a wrapper to a Pod with additional pre-computed information to
