af0b596ac12428ddd90129e7ac02c738fc32e39b
lcw729 <lcw729729@gmail.com>
2022-03-21 18:19:33 +0900

    scheduler get metrics

--- hcp-scheduler/pkg/scheduling.go
+++ hcp-scheduler/pkg/scheduling.go
@@ -1,19 +1,136 @@
 package pkg
 
 import (
+	"Hybrid_Cloud/hcp-scheduler/pkg/resourceinfo"
+	"Hybrid_Cloud/util/clusterManager"
 	"context"
 
 	"k8s.io/client-go/kubernetes"
 )
 
+// Scheduler watches for new unscheduled pods. It attempts to find
+// nodes that they fit on and writes bindings back to the api server.
 type Scheduler struct {
 	ClusterClients map[string]*kubernetes.Clientset
+	ClusterInfo    []*resourceinfo.ClusterInfo
 	SchdPolicy     string
 }
 
-func New() {
-
+func NewScheduler(
+	clustercClients map[string]*kubernetes.Clientset,
+	clusterInfo *[]resourceinfo.ClusterInfo,
+	SchdPolicy string) *Scheduler {
+	cm := clusterManager.NewClusterManager()
+	schd := Scheduler{
+		ClusterClients: cm.Cluster_kubeClients,
+		ClusterInfo:    resourceinfo.NewClusterInfoList(),
+	}
+	return &schd
 }
+
+/*
+// New returns a Scheduler
+func New(client clientset.Interface,
+	informerFactory informers.SharedInformerFactory,
+	dynInformerFactory dynamicinformer.DynamicSharedInformerFactory,
+	recorderFactory profile.RecorderFactory,
+	stopCh <-chan struct{},
+	opts ...Option) (*Scheduler, error) {
+
+	stopEverything := stopCh
+	if stopEverything == nil {
+		stopEverything = wait.NeverStop
+	}
+
+	options := defaultSchedulerOptions
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	if options.applyDefaultProfile {
+		var versionedCfg v1beta3.KubeSchedulerConfiguration
+		scheme.Scheme.Default(&versionedCfg)
+		cfg := config.KubeSchedulerConfiguration{}
+		if err := scheme.Scheme.Convert(&versionedCfg, &cfg, nil); err != nil {
+			return nil, err
+		}
+		options.profiles = cfg.Profiles
+	}
+
+	registry := frameworkplugins.NewInTreeRegistry()
+	if err := registry.Merge(options.frameworkOutOfTreeRegistry); err != nil {
+		return nil, err
+	}
+
+	metrics.Register()
+
+	extenders, err := buildExtenders(options.extenders, options.profiles)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't build extenders: %w", err)
+	}
+
+	podLister := informerFactory.Core().V1().Pods().Lister()
+	nodeLister := informerFactory.Core().V1().Nodes().Lister()
+
+	// The nominator will be passed all the way to framework instantiation.
+	nominator := internalqueue.NewPodNominator(podLister)
+	snapshot := internalcache.NewEmptySnapshot()
+	clusterEventMap := make(map[framework.ClusterEvent]sets.String)
+
+	profiles, err := profile.NewMap(options.profiles, registry, recorderFactory,
+		frameworkruntime.WithComponentConfigVersion(options.componentConfigVersion),
+		frameworkruntime.WithClientSet(client),
+		frameworkruntime.WithKubeConfig(options.kubeConfig),
+		frameworkruntime.WithInformerFactory(informerFactory),
+		frameworkruntime.WithSnapshotSharedLister(snapshot),
+		frameworkruntime.WithPodNominator(nominator),
+		frameworkruntime.WithCaptureProfile(frameworkruntime.CaptureProfile(options.frameworkCapturer)),
+		frameworkruntime.WithClusterEventMap(clusterEventMap),
+		frameworkruntime.WithParallelism(int(options.parallelism)),
+		frameworkruntime.WithExtenders(extenders),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("initializing profiles: %v", err)
+	}
+
+	if len(profiles) == 0 {
+		return nil, errors.New("at least one profile is required")
+	}
+
+	podQueue := internalqueue.NewSchedulingQueue(
+		profiles[options.profiles[0].SchedulerName].QueueSortFunc(),
+		informerFactory,
+		internalqueue.WithPodInitialBackoffDuration(time.Duration(options.podInitialBackoffSeconds)*time.Second),
+		internalqueue.WithPodMaxBackoffDuration(time.Duration(options.podMaxBackoffSeconds)*time.Second),
+		internalqueue.WithPodNominator(nominator),
+		internalqueue.WithClusterEventMap(clusterEventMap),
+		internalqueue.WithPodMaxUnschedulableQDuration(options.podMaxUnschedulableQDuration),
+	)
+
+	schedulerCache := internalcache.New(durationToExpireAssumedPod, stopEverything)
+
+	// Setup cache debugger.
+	debugger := cachedebugger.New(nodeLister, podLister, schedulerCache, podQueue)
+	debugger.ListenForSignal(stopEverything)
+
+	sched := newScheduler(
+		schedulerCache,
+		extenders,
+		internalqueue.MakeNextPodFunc(podQueue),
+		MakeDefaultErrorFunc(client, podLister, podQueue, schedulerCache),
+		stopEverything,
+		podQueue,
+		profiles,
+		client,
+		snapshot,
+		options.percentageOfNodesToScore,
+	)
+
+	addAllEventHandlers(sched, informerFactory, dynInformerFactory, unionedGVKs(clusterEventMap))
+
+	return sched, nil
+}
+*/
 
 // scheduleOne does the entire scheduling workflow for a single pod. It is serialized on the scheduling algorithm's host fitting.
 func (sched *Scheduler) scheduleOne(ctx context.Context) {
