27b957f8aff7814705ac950c913cc6f82c0ff3aa
lcw729 <lcw729729@gmail.com>
2022-04-13 15:50:23 +0900

    hcp init setting shell script

---
+++ setting/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install kubefed
+./create_cluster/4.get_kubefedctl.sh
+./create_cluster/5.install_helm.sh
+./create_cluster/6.install_fedaration.sh
+
+# create CRD
+kubectl apply -f /root/go/src/Hybrid_Cloud/pkg/crds
+
