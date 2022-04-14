a89761996ae26db8ffcda652bdb6108b359875c9
lcw729 <lcw729729@gmail.com>
2022-04-14 16:14:14 +0900

    update scheduling function : return a slice containing the scheduling result

--- hcp-scheduler/pkg/scheduler/scheduling.go
+++ hcp-scheduler/pkg/scheduler/scheduling.go
@@ -21,13 +21,25 @@ type Scheduler struct {
 	SchdPolicy     string
 }
 
+func ListTargetClusters() []string {
+	cm, _ := clusterManager.NewClusterManager()
+	kubefed_clusters := cm.Cluster_list.Items
+	var target_clusters []string
+	for _, i := range kubefed_clusters {
+		target_clusters = append(target_clusters, i.ObjectMeta.Name)
+	}
+	return target_clusters
+}
+
 func NewScheduler() *Scheduler {
 	cm, _ := clusterManager.NewClusterManager()
 
 	schd := Scheduler{
 		ClusterClients: cm.Cluster_kubeClients,
 		ClusterInfo:    resourceinfo.NewClusterInfoList(),
+		ClusterList:    ListTargetClusters(),
 	}
+
 	// HCPPolicy 최적 배치 알고리즘 정책 읽어오기
 	algorithm, err := policy.GetAlgorithm()
 	if err == nil {
@@ -36,46 +48,54 @@ func NewScheduler() *Scheduler {
 		schd.SchdPolicy = "DEFAULT_SCHEDPOLICY"
 	}
 
-	schd.ClusterList = []string{"hcp-cluster", "aks-master"}
-
 	return &schd
 }
 
 func (s *Scheduler) Scheduling(deployment *v1alpha1.HCPDeployment) []v1alpha1.Target {
-	schedule_type := s.SchdPolicy
-	replicas := deployment.Spec.RealDeploymentSpec.Replicas
 
-	fmt.Println(int(*replicas))
-	for i := 0; i < int(*replicas); i++ {
+	schedule_type := s.SchdPolicy
+	replicas := *deployment.Spec.RealDeploymentSpec.Replicas
+	var scheduling_result []v1alpha1.Target
+	var cnt int32 = 0
+
+	for i := 0; i < int(replicas); i++ {
 		switch schedule_type {
 		case "Affinity":
 			target := algorithm.Affinity(&s.ClusterList)
-			if registerTarget(deployment, target) {
-				fmt.Printf("success to scheduler %d/%d pod to %s\n", i+1, int(*replicas), target)
+			if target != "" {
+				if updateSchedulingResult(&scheduling_result, target) {
+					cnt += 1
+					fmt.Printf("[Scheduling] %d/%d pod / TargetCluster : %s\n", i+1, replicas, target)
+				}
 			} else {
-				fmt.Println("fail to scheduling")
+				fmt.Println("ERROR: No cluster to be scheduled")
+				fmt.Println("Scheduling failed")
 				return nil
 			}
 		case "DRF":
 		}
 	}
-	return deployment.Spec.SchedulingResult.Targets
+
+	if cnt == replicas {
+		fmt.Println("Scheduling succeeded")
+		fmt.Println("[Scheduling Result] =====> ", scheduling_result[0].Cluster, *scheduling_result[0].Replicas)
+		return scheduling_result
+	} else {
+		fmt.Println("Scheduling failed")
+		return nil
+	}
 }
 
-func registerTarget(resource *v1alpha1.HCPDeployment, cluster string) bool {
+func updateSchedulingResult(scheduling_result *[]v1alpha1.Target, cluster string) bool {
 
-	targets := resource.Spec.SchedulingResult.Targets
-
-	for i, target := range targets {
+	for i, target := range *scheduling_result {
 		// 이미 target cluster 목록에 cluster가 있는 경우
 		if target.Cluster == cluster {
 			// replicas 개수 증가
 			temp := *target.Replicas
 			temp += 1
 			target.Replicas = &temp
-			targets[i] = target
-			resource.Spec.SchedulingResult.Targets = targets
-			fmt.Println(resource.Spec.SchedulingResult.Targets)
+			(*scheduling_result)[i] = target
 			return true
 		}
 	}
@@ -83,122 +103,14 @@ func registerTarget(resource *v1alpha1.HCPDeployment, cluster string) bool {
 	// target cluster 목록에 cluster가 없는 경우
 
 	// replicas 개수 1로 설정
-	new_target := new(v1alpha1.Target)
+	var new_target v1alpha1.Target
 	new_target.Cluster = cluster
 	var one int32 = 1
 	new_target.Replicas = &one
-	targets = append(targets, *new_target)
-	resource.Spec.SchedulingResult.Targets = targets
-	fmt.Println(resource.Spec.SchedulingResult.Targets)
-
+	*scheduling_result = append((*scheduling_result), new_target)
 	return true
 }
 
-/*
-// New returns a Scheduler
-func New(client clientset.Interface,
-	informerFactory informers.SharedInformerFactory,
-	dynInformerFactory dynamicinformer.DynamicSharedInformerFactory,
-	recorderFactory profile.RecorderFactory,
-	stopCh <-chan struct{},
-	opts ...Option) (*Scheduler, error) {
-
-	stopEverything := stopCh
-	if stopEverything == nil {
-		stopEverything = wait.NeverStop
-	}
-
-	options := defaultSchedulerOptions
-	for _, opt := range opts {
-		opt(&options)
-	}
-
-	if options.applyDefaultProfile {
-		var versionedCfg v1beta3.KubeSchedulerConfiguration
-		scheme.Scheme.Default(&versionedCfg)
-		cfg := config.KubeSchedulerConfiguration{}
-		if err := scheme.Scheme.Convert(&versionedCfg, &cfg, nil); err != nil {
-			return nil, err
-		}
-		options.profiles = cfg.Profiles
-	}
-
-	registry := frameworkplugins.NewInTreeRegistry()
-	if err := registry.Merge(options.frameworkOutOfTreeRegistry); err != nil {
-		return nil, err
-	}
-
-	metrics.Register()
-
-	extenders, err := buildExtenders(options.extenders, options.profiles)
-	if err != nil {
-		return nil, fmt.Errorf("couldn't build extenders: %w", err)
-	}
-
-	podLister := informerFactory.Core().V1().Pods().Lister()
-	nodeLister := informerFactory.Core().V1().Nodes().Lister()
-
-	// The nominator will be passed all the way to framework instantiation.
-	nominator := internalqueue.NewPodNominator(podLister)
-	snapshot := internalcache.NewEmptySnapshot()
-	clusterEventMap := make(map[framework.ClusterEvent]sets.String)
-
-	profiles, err := profile.NewMap(options.profiles, registry, recorderFactory,
-		frameworkruntime.WithComponentConfigVersion(options.componentConfigVersion),
-		frameworkruntime.WithClientSet(client),
-		frameworkruntime.WithKubeConfig(options.kubeConfig),
-		frameworkruntime.WithInformerFactory(informerFactory),
-		frameworkruntime.WithSnapshotSharedLister(snapshot),
-		frameworkruntime.WithPodNominator(nominator),
-		frameworkruntime.WithCaptureProfile(frameworkruntime.CaptureProfile(options.frameworkCapturer)),
-		frameworkruntime.WithClusterEventMap(clusterEventMap),
-		frameworkruntime.WithParallelism(int(options.parallelism)),
-		frameworkruntime.WithExtenders(extenders),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("initializing profiles: %v", err)
-	}
-
-	if len(profiles) == 0 {
-		return nil, errors.New("at least one profile is required")
-	}
-
-	podQueue := internalqueue.NewSchedulingQueue(
-		profiles[options.profiles[0].SchedulerName].QueueSortFunc(),
-		informerFactory,
-		internalqueue.WithPodInitialBackoffDuration(time.Duration(options.podInitialBackoffSeconds)*time.Second),
-		internalqueue.WithPodMaxBackoffDuration(time.Duration(options.podMaxBackoffSeconds)*time.Second),
-		internalqueue.WithPodNominator(nominator),
-		internalqueue.WithClusterEventMap(clusterEventMap),
-		internalqueue.WithPodMaxUnschedulableQDuration(options.podMaxUnschedulableQDuration),
-	)
-
-	schedulerCache := internalcache.New(durationToExpireAssumedPod, stopEverything)
-
-	// Setup cache debugger.
-	debugger := cachedebugger.New(nodeLister, podLister, schedulerCache, podQueue)
-	debugger.ListenForSignal(stopEverything)
-
-	sched := newScheduler(
-		schedulerCache,
-		extenders,
-		internalqueue.MakeNextPodFunc(podQueue),
-		MakeDefaultErrorFunc(client, podLister, podQueue, schedulerCache),
-		stopEverything,
-		podQueue,
-		profiles,
-		client,
-		snapshot,
-		options.percentageOfNodesToScore,
-	)
-
-	addAllEventHandlers(sched, informerFactory, dynInformerFactory, unionedGVKs(clusterEventMap))
-
-	return sched, nil
-}
-*/
-
-// scheduleOne does the entire scheduling workflow for a single pod. It is serialized on the scheduling algorithm's host fitting.
 func (sched *Scheduler) scheduleOne(ctx context.Context) {
 
 }
