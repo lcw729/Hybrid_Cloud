0d79970b4a698205c6f3706b690f490caffcc062
lcw729 <lcw729729@gmail.com>
2022-04-18 18:10:26 +0900

    update scheduler struct

--- pkg/scheduler/scheduling.go
+++ pkg/scheduler/scheduling.go
@@ -3,6 +3,7 @@ package scheduler
 import (
 	policy "Hybrid_Cloud/hcp-resource/hcppolicy"
 	"Hybrid_Cloud/hcp-scheduler/pkg/algorithm/priorities"
+	"Hybrid_Cloud/hcp-scheduler/pkg/internal/scoretable"
 	"Hybrid_Cloud/hcp-scheduler/pkg/resourceinfo"
 	"Hybrid_Cloud/pkg/apis/resource/v1alpha1"
 	"Hybrid_Cloud/util/clusterManager"
@@ -17,11 +18,20 @@ import (
 // Scheduler watches for new unscheduled pods. It attempts to find
 // nodes that they fit on and writes bindings back to the api server.
 type Scheduler struct {
-	ClusterClients  map[string]*kubernetes.Clientset
-	ClusterInfoList resourceinfo.ClusterInfoList
-	SchdPolicy      string
+	SchedulingResource v1.Pod
+	ClusterClients     map[string]*kubernetes.Clientset
+	ClusterInfoList    *resourceinfo.ClusterInfoList
+	ScoreTable         *map[string]float32
+	SchdPolicy         string
 }
 
+var AlgorithmMap = map[string]func(*v1.Pod, *v1.Node) int32{
+
+	"Affinity":        priorities.NodeAffinity,
+	"TaintToleration": priorities.TaintToleration,
+}
+
+/*
 func ListTargetClusters() []string {
 	cm, _ := clusterManager.NewClusterManager()
 	kubefed_clusters := cm.Cluster_list.Items
@@ -31,89 +41,7 @@ func ListTargetClusters() []string {
 	}
 	return target_clusters
 }
-
-func createTestNodes() []*NodeInfo {
-	// Test
-	label1 := map[string]string{"foo": "bar"}
-	label2 := map[string]string{"key": "value"}
-	label3 := map[string]string{"az": "az1"}
-	// label4 := map[string]string{"abc": "az11", "def": "az22"}
-	// label5 := map[string]string{"foo": "bar", "key": "value", "az": "az1"}
-
-	affinity1 := &v1.Affinity{
-		NodeAffinity: &v1.NodeAffinity{
-			PreferredDuringSchedulingIgnoredDuringExecution: []v1.PreferredSchedulingTerm{{
-				Weight: 2,
-				Preference: v1.NodeSelectorTerm{
-					MatchExpressions: []v1.NodeSelectorRequirement{{
-						Key:      "foo",
-						Operator: v1.NodeSelectorOpIn,
-						Values:   []string{"bar"},
-					}},
-				},
-			}},
-		},
-	}
-	_ = affinity1
-	affinity2 := &v1.Affinity{
-		NodeAffinity: &v1.NodeAffinity{
-			PreferredDuringSchedulingIgnoredDuringExecution: []v1.PreferredSchedulingTerm{
-				{
-					Weight: 2,
-					Preference: v1.NodeSelectorTerm{
-						MatchExpressions: []v1.NodeSelectorRequirement{
-							{
-								Key:      "foo",
-								Operator: v1.NodeSelectorOpIn,
-								Values:   []string{"bar"},
-							},
-						},
-					},
-				},
-				{
-					Weight: 4,
-					Preference: v1.NodeSelectorTerm{
-						MatchExpressions: []v1.NodeSelectorRequirement{
-							{
-								Key:      "key",
-								Operator: v1.NodeSelectorOpIn,
-								Values:   []string{"value"},
-							},
-						},
-					},
-				},
-				{
-					Weight: 5,
-					Preference: v1.NodeSelectorTerm{
-						MatchExpressions: []v1.NodeSelectorRequirement{
-							{
-								Key:      "foo",
-								Operator: v1.NodeSelectorOpIn,
-								Values:   []string{"bar"},
-							},
-							{
-								Key:      "key",
-								Operator: v1.NodeSelectorOpIn,
-								Values:   []string{"value"},
-							},
-							{
-								Key:      "az",
-								Operator: v1.NodeSelectorOpIn,
-								Values:   []string{"az1"},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	schd.ClusterInfoList. []*v1.Node{
-		{ObjectMeta: metav1.ObjectMeta{Name: "machine1", Labels: label1}},
-		{ObjectMeta: metav1.ObjectMeta{Name: "machine2", Labels: label2}},
-		{ObjectMeta: metav1.ObjectMeta{Name: "machine3", Labels: label3}},
-	},
-}
+*/
 
 func NewScheduler() *Scheduler {
 	cm, _ := clusterManager.NewClusterManager()
@@ -135,64 +63,45 @@ func NewScheduler() *Scheduler {
 	return &schd
 }
 
-type resource struct {
-	pod   *v1.Pod
-	nodes []*v1.Node
-	name  string
-}
-
 func (sched *Scheduler) Scheduling(deployment *v1alpha1.HCPDeployment) []v1alpha1.Target {
-
-	test := resource{
-		pod: &v1.Pod{
-			ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{},
-			},
-			Spec: v1.PodSpec{
-				Affinity: affinity2,
-			},
-		},
-		nodes: []*v1.Node{
-			{ObjectMeta: metav1.ObjectMeta{Name: "machine1", Labels: label1}},
-			{ObjectMeta: metav1.ObjectMeta{Name: "machine2", Labels: label2}},
-			{ObjectMeta: metav1.ObjectMeta{Name: "machine3", Labels: label3}},
-		},
-
-		name: "all machines are same priority as NodeAffinity is nil",
-	}
 
 	schedule_type := sched.SchdPolicy
 	replicas := *deployment.Spec.RealDeploymentSpec.Replicas
 	var scheduling_result []v1alpha1.Target
 	var cnt int32 = 0
 
+	// set schedulingResource
 	fake_pod := newPodFromHCPDeployment(deployment)
+	sched.SchedulingResource = *fake_pod
+
+	// set scoretable
+	sched.ScoreTable = scoretable.NewScoreTable(len(*sched.ClusterInfoList))
 
 	for i := 0; i < int(replicas); i++ {
-		switch schedule_type {
-		case "Affinity":
-			/*
-				target := algorithm.Affinity(&s.ClusterList)
-				if target != "" {
-					if updateSchedulingResult(&scheduling_result, target) {
-						cnt += 1
-						fmt.Printf("[Scheduling] %d/%d pod / TargetCluster : %s\n", i+1, replicas, target)
-					}
-				} else {
-					fmt.Println("ERROR: No cluster to be scheduled")
-					fmt.Println("Scheduling failed")
-					return nil
+		sched.Scoring(schedule_type)
+		score_table := sched.ScoreTable
+		if score_table != nil {
+			target := scoretable.SortScore(*score_table)[0].Cluster
+			if target != "" {
+				if updateSchedulingResult(&scheduling_result, target) {
+					cnt += 1
+					fmt.Printf("[Scheduling] %d/%d pod / TargetCluster : %s\n", i+1, replicas, target)
+					fmt.Println()
 				}
-			*/
-			score_table := priorities.NodeAffinity(fake_pod, &sched.ClusterInfoList)
-			fmt.Println(score_table.SortScore())
-		case "DRF":
+			} else {
+				fmt.Println("ERROR: No cluster to be scheduled")
+				fmt.Println("Scheduling failed")
+				break
+			}
+		} else {
+			fmt.Println("Scheduling failed")
+			return nil
 		}
 	}
 
 	if cnt == replicas {
 		fmt.Println("Scheduling succeeded")
-		fmt.Println("[Scheduling Result] =====> ", scheduling_result[0].Cluster, *scheduling_result[0].Replicas)
+		printSchedulingResult(scheduling_result)
 		return scheduling_result
 	} else {
 		fmt.Println("Scheduling failed")
@@ -200,10 +109,20 @@ func (sched *Scheduler) Scheduling(deployment *v1alpha1.HCPDeployment) []v1alpha
 	}
 }
 
+func printSchedulingResult(targets []v1alpha1.Target) {
+	fmt.Println("========scheduling result========")
+	for _, i := range targets {
+		fmt.Println("target cluster :", i.Cluster)
+		fmt.Println("replicas       :", *i.Replicas)
+		fmt.Println()
+	}
+}
+
 func newPodFromHCPDeployment(deployment *v1alpha1.HCPDeployment) *v1.Pod {
 
 	pod := &v1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
+			Name:        deployment.GetObjectMeta().GetName() + "-pod",
 			Annotations: deployment.Annotations,
 			Labels:      deployment.Labels,
 		},
@@ -236,6 +155,32 @@ func updateSchedulingResult(scheduling_result *[]v1alpha1.Target, cluster string
 	new_target.Replicas = &one
 	*scheduling_result = append((*scheduling_result), new_target)
 	return true
+}
+
+func (sched *Scheduler) Scoring(algorithm string) {
+
+	var pod = &sched.SchedulingResource
+	clusterinfoList := sched.ClusterInfoList
+	var score int32
+
+	for _, clusterinfo := range *clusterinfoList {
+		fmt.Println("==>", clusterinfo.ClusterName)
+		score = 0
+		node_list := (*clusterinfo).Nodes
+
+		for _, node := range node_list {
+			node_score := AlgorithmMap[algorithm](pod, node.Node)
+			if node_score == -1 {
+				fmt.Println("fail to scoring node")
+				return
+			} else {
+				fmt.Println(node.NodeName, "score :", node_score)
+				score += node_score
+			}
+		}
+		fmt.Println("*", clusterinfo.ClusterName, "total score :", score)
+		(*sched.ScoreTable)[clusterinfo.ClusterName] = float32(score)
+	}
 }
 
 func (sched *Scheduler) scheduleOne(ctx context.Context) {
