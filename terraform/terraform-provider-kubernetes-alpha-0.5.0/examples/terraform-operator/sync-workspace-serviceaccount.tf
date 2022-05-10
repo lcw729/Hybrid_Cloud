30c272fc9e2436d3373fedf0c6617ece0c913fa7
lcw729 <lcw729729@gmail.com>
2022-05-10 13:15:08 +0900

    check security

--- terraform/terraform-provider-kubernetes-alpha-0.5.0/examples/terraform-operator/sync-workspace-serviceaccount.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_service_account" "tfc-service-account" {
-  metadata {
-    name      = "${kubernetes_manifest.namespace.object.metadata.name}-sync-workspace"
-    namespace = kubernetes_manifest.namespace.object.metadata.name
-    labels = {
-      app = kubernetes_manifest.namespace.object.metadata.name
-    }
-  }
-}
