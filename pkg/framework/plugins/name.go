e08cca1617851a6269ebd206ffb2256f267d6b26
lcw729 <lcw729729@gmail.com>
2022-05-24 12:01:54 +0900

    .

---
+++ pkg/framework/plugins/name.go
@@ -0,0 +1,12 @@
+package plugins
+
+const (
+	NodeName          = "NodeName"
+	NodePorts         = "NodePorts"
+	NodeResourcesFit  = "NodeResourcesFit"
+	NodeUnschedulable = "NodeUnschedulable"
+	BalanceAllocation = "BalancedAllocation"
+	ImageLocality     = "ImageLocality"
+	TaintToleration   = "TaintToleration"
+	NodeAffinity      = "NodeAffinity"
+)
--- pkg/scheduler/main/main.go
+++ pkg/scheduler/main/main.go
@@ -1,30 +1,26 @@
 package main
 
-import (
-	"fmt"
-
-	v1 "k8s.io/api/core/v1"
-)
+import "Hybrid_Cloud/hcp-scheduler/pkg/scheduler"
 
 func main() {
-	pod := &v1.Pod{
-		Spec: v1.PodSpec{
-			Volumes: []v1.Volume{
-				{
-					VolumeSource: v1.VolumeSource{
-						HostPath: &v1.HostPathVolumeSource{
-							Path: "/test",
-						},
-					},
-				},
-			},
-		},
-	}
+	// pod := &v1.Pod{
+	// 	Spec: v1.PodSpec{
+	// 		Volumes: []v1.Volume{
+	// 			{
+	// 				VolumeSource: v1.VolumeSource{
+	// 					HostPath: &v1.HostPathVolumeSource{
+	// 						Path: "/test",
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
 
-	podVols := pod.Spec.Volumes
-	for _, podVol := range podVols {
-		fmt.Println(podVol.AWSElasticBlockStore)
-	}
-	// sched := scheduler.NewScheduler()
-	// sched.Scheduling(nil)
+	// podVols := pod.Spec.Volumes
+	// for _, podVol := range podVols {
+	// 	fmt.Println(podVol.AWSElasticBlockStore)
+	// }
+	sched := scheduler.NewScheduler()
+	sched.Scheduling(nil)
 }
