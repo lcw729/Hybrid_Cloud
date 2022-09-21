6501ed9116a5982f292578d3ebf36e4bcfa6ce4e
yjb <happybean75@naver.com>
2022-09-21 15:07:53 +0900

    v1

---
+++ hcp-resource/hcpcluster/find.go
@@ -0,0 +1,26 @@
+package hcpcluster
+
+import (
+	hcpclusterv1alpha1 "Hybrid_Cloud/pkg/client/hcpcluster/v1alpha1/clientset/versioned"
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+)
+
+func FindHCPClusterList(clientset *hcpclusterv1alpha1.Clientset, cluster string) bool {
+	cluster_list, err := clientset.HcpV1alpha1().HCPClusters("hcp").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		klog.Errorln(err)
+	} else {
+		for _, c := range cluster_list.Items {
+			if c.ObjectMeta.Name == cluster {
+				klog.Info("Find %s in HCPClusterList\n", cluster)
+				return true
+			}
+		}
+	}
+	klog.Info("Fail to find %s in HCPClusterList\n", cluster)
+	klog.Info("You should register your cluster to HCP\n")
+	return false
+}
