be7b1d5fe72cc9b84aba1b95c7cf84df267d8ba1
lcw729 <lcw729729@gmail.com>
2022-09-29 17:40:28 +0900

    n

--- before_push_hybrid_cloud.sh
+++ before_push_hybrid_cloud.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-for dir in */; do mkdir -- "$dir/git"; done
+for dir in */; do mkdir -- "${dir}git"; done
 
-for dir in */; do mv "$dir/.git" "$dir/git"; done
-
-git rm -r --cached
+for dir in */; do mv "${dir}.git" "${dir}git"; done
